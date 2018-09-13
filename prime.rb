183c019b068c09181aa1d1ed8a13767ccae2963a
Gina Fedder <ginafedder@yahoo.com>
2018-09-13 20:27:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
-def prime?(integer)
-  require 'prime'
-  Prime.prime?(integer)
-end
+
 
 def prime?(integer)
   array = [1..50]
-  array.each {|i| 
+  array.each do |i| 
+    i / 1 && i / i
+    return true
+  end
+end
